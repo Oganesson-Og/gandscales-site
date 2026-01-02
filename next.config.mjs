15a108479c00128bea00469dd548d8f4069ca003
Keith Satuku <67236403+Oganesson-Og@users.noreply.github.com>
2026-01-02 18:41:33 +0200

    Add basePath and assetPrefix to next.config.mjs
    Fix Next.js for a GitHub Pages subpath (VERY important)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
+const repo = "gandscales-site";
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-}
+  images: { unoptimized: true },
 
-export default nextConfig
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+};
 
+export default nextConfig;
