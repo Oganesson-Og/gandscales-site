4a58e31f842bce581fd7a02a7a9b0f463a6da670
Keith Satuku <67236403+Oganesson-Og@users.noreply.github.com>
2026-01-02 18:56:06 +0200

    adding image paths
    Adding image paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-const repo = "gandscales-site";
-const isProd = process.env.NODE_ENV === "production";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
 
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
+  basePath: "/gandscales-site",
+  assetPrefix: "/gandscales-site/",
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/gandscales-site",
+  },
 };
 
 export default nextConfig;
