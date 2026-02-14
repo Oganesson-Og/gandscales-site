fc16289beed888a479da8ace0cd4445d78e9237b
Keith Satuku <67236403+Oganesson-Og@users.noreply.github.com>
2026-02-14 17:05:01 +0200

    Add basePath and assetPrefix to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,14 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+
   ...(normalizedBasePath
     ? {
         basePath: normalizedBasePath,
         assetPrefix: `${normalizedBasePath}/`,
       }
     : {}),
+
   env: {
     NEXT_PUBLIC_BASE_PATH: normalizedBasePath,
     NEXT_PUBLIC_SITE_URL:
